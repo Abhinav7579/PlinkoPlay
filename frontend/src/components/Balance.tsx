b029e844cf0f60a18f7a0bce48b49203301463b5
Abhinav <abhinavsyunary@gmail.com>
2025-08-16 15:59:05 +0530

    balance fetched

--- frontend/src/components/Balance.tsx
+++ frontend/src/components/Balance.tsx
@@ -1,5 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 const Balance= () => {
+  const [balance,setbalance]=useState("");
+   useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/api/v1/user/account/balance", {
+          headers: {
+            Authorization:`Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        setbalance(res.data.balance);
+      } catch (error) {
+        console.error("Error fetching balance:", error);
+      }
+    };
+
+    fetchBalance();
+  }, []); 
+
   return (
     
     <div className="fixed top-[74px] left-1/2 transform -translate-x-1/2 z-50">
@@ -8,7 +28,7 @@ const Balance= () => {
       <p className="text-white text-xl">Wallet</p>
     </div>
     <div className="bg-white w-[120px] h-[40px] flex items-center justify-center">
-      <p className="text-black text-xl">₹ 500000</p>
+      <p className="text-black text-xl">₹ {Number(balance).toFixed(2)}</p>
     </div>
   </div>
 </div>
