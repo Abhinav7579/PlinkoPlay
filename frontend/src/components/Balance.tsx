a049c0e760bec44eac8760c39840cd93223d6de5
Abhinav <abhinavsyunary@gmail.com>
2025-08-16 16:56:44 +0530

    balacne section color changed

--- frontend/src/components/Balance.tsx
+++ frontend/src/components/Balance.tsx
@@ -24,11 +24,11 @@ const Balance= () => {
     
     <div className="fixed top-[74px] left-1/2 transform -translate-x-1/2 z-50">
   <div className="flex space-x-0">
-    <div className="bg-green-500 w-[100px] h-[40px] flex items-center justify-center">
+    <div className="bg-blue-800 w-[100px] h-[40px] flex items-center justify-center">
       <p className="text-white text-xl">Wallet</p>
     </div>
-    <div className="bg-white w-[120px] h-[40px] flex items-center justify-center">
-      <p className="text-black text-xl">₹ {Number(balance).toFixed(2)}</p>
+    <div className="bg-gray-900 w-[120px] h-[40px] flex items-center justify-center">
+      <p className="text-white text-xl">₹ {Number(balance).toFixed(2)}</p>
     </div>
   </div>
 </div>
